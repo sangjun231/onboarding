69f5ca8bd5c68cc43319bc1aa3b0511e7b0b4678
sangjunShin <987654321123sin@gmail.com>
2024-10-08 12:30:00 +0900

    Feat: 프로젝트 세팅
    기본 폴더 구조 생성 및 캐싱 처리를 위한 tanstack Query 설정, tailwind 설정, Router 기본만 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import QueryProvider from "./QueryProvider";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <QueryProvider>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryProvider>
+  </React.StrictMode>
+);
