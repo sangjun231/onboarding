19ec598357bcf710f0859cf23441fe8cf0409b4c
sangjunShin <987654321123sin@gmail.com>
2024-10-09 14:22:41 +0900

    Feat: 인가 여부를 위해 Router 추가

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,10 +1,63 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import Layout from "../components/Layout";
+import MainPage from "../pages/MainPage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import MyPage from "../pages/MyPage";
+import { useUserStore } from "../stores/userStore";
+
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const accessToken = useUserStore((state) => state.accessToken);
+
+  if (!accessToken) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
+
+const PublicRoute = ({ children }: { children: JSX.Element }) => {
+  const accessToken = useUserStore((state) => state.accessToken);
+
+  if (accessToken) {
+    return <Navigate to="/" />;
+  }
+  return children;
+};
 
 const Router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <MainPage />,
+      },
+      {
+        path: "/MyPage",
+        element: (
+          <ProtectedRoute>
+            <MyPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/login",
+        element: (
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        ),
+      },
+      {
+        path: "/register",
+        element: (
+          <PublicRoute>
+            <RegisterPage />
+          </PublicRoute>
+        ),
+      },
+    ],
   },
 ]);
 
